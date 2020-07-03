9db0f18d42ef2904fad19a4f7b71ccd1b6dbb997
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-03 01:09:12 -0600

    export a function to get a path-returning promise

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const glob = require('glob');
 
-glob('/nix/store/*-electron-[678].?.??(?)/', function (er, files) {
-	if (er || files.length == 0) {
-		console.warn('No electron found in the nix store; falling back to npm');
-		return require('electron');
-	} else if (files.length > 1) {
-		console.warn('More than one electron found in the nix store; falling back to npm', files);
-		return require('electron');
-	} else {
-		console.warn('Unique electron found in the nix store; using it');
-		return files[0] + '/bin/electron';
-	}
-});
+module.exports = {
+    getElectronPath: function () {
+	return new Promise(function(resolve) {
+	     glob('/nix/store/*-electron-[678].?.??(?)/', function (er, files) {
+			if (er || files.length == 0) {
+			console.warn('No electron found in the nix store; falling back to npm');
+			resolve(require('electron'));
+		} else if (files.length > 1) {
+			console.warn('More than one electron found in the nix store; falling back to npm', files);
+			resolve(require('electron'));
+		} else {
+			console.warn('Unique electron found in the nix store; using it');
+			resolve(files[0] + '/bin/electron');
+		}
+    	    });
+	});
+    }
+};
+
