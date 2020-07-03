cbc58382952885a8787d48ac4494ecc355c984fa
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-02 21:55:27 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const glob = require('glob');
+
+glob('/nix/store/*-electron_*/', function (er, files) {
+	if (er || files.length == 0) {
+		console.warn('No electron found in the nix store; falling back to npm');
+		return require('electron');
+	} else if (files.length > 0) {
+		console.warn('More than one electron found in the nix store; falling back to npm');
+		return require('electron');
+	} else {
+		console.warn('Unique electron found in the nix store; using it');
+		return files[0];
+	}
+});
