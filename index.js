ebb156f51b145824ef1cae731d807f11bd5ba976
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-02 23:57:42 -0600

    increase glob specificity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const glob = require('glob');
 
-glob('/nix/store/*-electron-?.?.??(?)/', function (er, files) {
+glob('/nix/store/*-electron-[678].?.??(?)/', function (er, files) {
 	if (er || files.length == 0) {
 		console.warn('No electron found in the nix store; falling back to npm');
 		return require('electron');
