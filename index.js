1224b13edeb7574b437a5c1f4fbd84e01a51d74a
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-03 00:22:18 -0600

    update debug print

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ glob('/nix/store/*-electron-[678].?.??(?)/', function (er, files) {
 		return require('electron');
 	} else if (files.length > 1) {
 		console.warn('More than one electron found in the nix store; falling back to npm');
-		console.log(files);
+		console.warn(files);
 		return require('electron');
 	} else {
 		console.warn('Unique electron found in the nix store; using it');
